b3f057213cf0e6cefa044b0c8c810e12c39a9dc8
Hayley Swimelar <hayley@linbit.com>
2015-11-06 16:30:43 -0800

    fix get_vol_nodes

--- datastore/drbd_common.sh
+++ datastore/drbd_common.sh
@@ -3,33 +3,45 @@
 # Return newline separted list of nodes that are assigned to a resource.
 get_vol_nodes () {
 
-ASSIGNMENTS=$(drbdmanage asignments -m --resources "$1" | awk -F',' '{ print $2 }')
+  ASSIGNMENTS_COMMAND="drbdmanage assignments -m --resources"
 
-  if [ASSIGNMENTS -eq ""]; then
+  if [ -n "$ASSIGNMENTS_COMMAND $1" ]; then
 
-    exit -1
+    # Wait for volume to be connected and deployed.
+    i=0
+    while [[ "$($ASSIGNMENTS_COMMAND $1 | awk -F',' '{ print $4 $5 }')" != "connect|deployconnect|deploy" ]]; do
+
+      sleep 1
+
+      if [ "$i" -gt 10 ]; then
+        exit -1
+      fi
+
+      ((i++))
+    done
+
+    echo "$($ASSIGNMENTS_COMMAND $1 | awk -F',' '{ print $1 }')"
 
   else
 
-    echo $ASSIGNMENTS
+    exit -1
 
   fi
-
 }
 
 # Return single node with a resource assigned to it.
 get_assignment_node () {
 
-    echo $(get_vol_nodes $1 | awk -F' ' '{ print $1 }' )
+  echo $(get_vol_nodes $1 | awk -F' ' '{ print $1 }' )
 
 }
 
 # Returns path to device node for a resouce.
 get_device_for_vol () {
 
-DRBD_MINOR="$(drbdmanage v -m -R "$1" | awk -F',' '{ print $6 }')"
+  DRBD_MINOR="$(drbdmanage v -m -R "$1" | awk -F',' '{ print $6 }')"
 
-echo $("/dev/$DRBD_MINOR_PREFIX$DRBD_MINOR")
+  echo $("/dev/$DRBD_MINOR_PREFIX$DRBD_MINOR")
 
 }
 
