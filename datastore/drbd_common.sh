5c0a0f4a1859944d83c9106f688072c5c479af1f
Hayley Swimelar <hayley@linbit.com>
2015-11-10 10:00:08 -0800

    add functions to add drbd resource

--- datastore/drbd_common.sh
+++ datastore/drbd_common.sh
@@ -70,3 +70,18 @@ get_device_for_res () {
 
 }
 
+# Check if resource exsists, returns resource name if it does.
+res_exsists () {
+  echo "$(drbdmanage list-resources --resources $1 -m | awk -F',' '{ print $1 }')"
+}
+# Add a resource and volume to drbd with a given size.
+add_vol () {
+
+  # Exit if resource already exsists.
+  if [ -n "$(res_exsists $1)" ]; then
+    exit -1
+  else
+    $(drbdmanage add-volume $1 $2)
+  fi
+
+}
