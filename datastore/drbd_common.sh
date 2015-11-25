0889b42616f3b4c1fefc3576c250f334bc53b534
Hayley Swimelar <hayley@linbit.com>
2015-11-25 08:49:35 -0800

    prefix function names with drbd

--- datastore/drbd_common.sh
+++ datastore/drbd_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Return newline separated list of nodes that are assigned to a resource.
-get_res_nodes () {
+drbd_get_res_nodes () {
 
   res_nodes="$(drbdmanage assignments -m --resources $1 | awk -F',' '{ print $1 }')"
 
@@ -13,14 +13,14 @@ get_res_nodes () {
 }
 
 # Return single node with a resource assigned to it.
-get_assignment_node () {
+drbd_get_assignment_node () {
 
-  echo $(get_res_nodes $1 | awk -F' ' '{ print $1 }' )
+  echo $(drbd_get_res_nodes $1 | awk -F' ' '{ print $1 }' )
 
 }
 
 # Check if resource is in connected and deployed on a single node.
-is_res_deployed () {
+drbd_is_res_deployed () {
 
   NODE_STATE="$(drbdmanage assignments -m --resources $1 --nodes $2 | awk -F',' '{ print $4, $5 }')"
 
@@ -33,12 +33,12 @@ is_res_deployed () {
 }
 
 # Wait until resource is deployed and connected on a single node.
-wait_res_deployed () {
+drbd_wait_res_deployed () {
 
   RETRY_LIMIT=10
 
 
-  until [ $(is_res_deployed $1 $2) -eq 0 ]; do
+  until [ $(drbd_is_res_deployed $1 $2) -eq 0 ]; do
     sleep 1
     if (( RETRY_LIMIT < 1 )); then
       exit -1
@@ -49,18 +49,18 @@ wait_res_deployed () {
 }
 
 # Wait until resource is deployed and connected on all nodes.
-wait_nodes_ready () {
+drbd_wait_nodes_ready () {
 
-  node_list=$(get_res_nodes $1)
+  node_list=$(drbd_get_res_nodes $1)
 
   for node in "${node_list[@]}"
   do
-    wait_res_deployed $1 $node
+    drbd_wait_res_deployed $1 $node
   done
 }
 
 # Returns path to device node for a resource.
-get_device_for_res () {
+drbd_get_device_for_res () {
 
   DRBD_MINOR="$(drbdmanage v -m -R "$1" | awk -F',' '{ print $6 }')"
 
@@ -69,14 +69,14 @@ get_device_for_res () {
 }
 
 # Check if resource exsists, returns resource name if it does.
-res_exsists () {
+drbd_res_exsists () {
   echo "$(drbdmanage list-resources --resources $1 -m | awk -F',' '{ print $1 }')"
 }
 # Add a resource to drbd with a given size.
-add_res () {
+drbd_add_res () {
 
   # Exit if resource already exsists.
-  if [ -n "$(res_exsists $1)" ]; then
+  if [ -n "$(drbd_res_exsists $1)" ]; then
     exit -1
   else
     $(drbdmanage add-volume $1 $2)
