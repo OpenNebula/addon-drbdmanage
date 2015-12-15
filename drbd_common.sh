4e75a17e4cc6e006cfd4a4db3594f829234d9a2b
Hayley Swimelar <hayley@linbit.com>
2015-12-15 09:52:50 -0800

    clean up whitespace

--- drbd_common.sh
+++ drbd_common.sh
@@ -2,7 +2,6 @@
 
 # Return newline separated list of nodes that are assigned to a resource.
 drbd_get_res_nodes () {
-
   res_name=$1
 
   res_nodes="$(drbdmanage assignments -m --resources $res_name awk -F',' '{ print $1 }')"
@@ -16,16 +15,13 @@ drbd_get_res_nodes () {
 
 # Return single node with a resource assigned to it.
 drbd_get_assignment_node () {
-
   res_name=$1
 
   echo $(drbd_get_res_nodes $res_name awk -F' ' '{ print $1 }' )
-
 }
 
 # Check if resource is in connected and deployed on a single node.
 drbd_is_res_deployed () {
-
   res_name=$1
   node_name=$2
   client_option=$3
@@ -43,12 +39,10 @@ drbd_is_res_deployed () {
   else
     echo 1
   fi
-
 }
 
 # Wait until resource is deployed and connected on a single node.
 drbd_wait_res_deployed () {
-
   res_name=$1
   node_name=$2
   client_option=$3
@@ -62,31 +56,26 @@ drbd_wait_res_deployed () {
     fi
     ((retries--))
   done
-
 }
 
 # Returns path to device node for a resource.
 drbd_get_device_for_res () {
-
   res_name=$1
 
   drbd_minor="$(drbdmanage v -m -R $res_name | awk -F',' '{ print $6 }')"
 
   echo "/dev/$DRBD_MINOR_PREFIX$drbd_minor"
-
 }
 
 # Check if resource exists, returns resource name if it does.
 drbd_res_exsists () {
-
   res_name=$1
 
   echo "$(drbdmanage list-resources --resources $res_name -m | awk -F',' '{ print $1 }')"
-
 }
+
 # Add a resource to drbd with a given size.
 drbd_add_res () {
-
   res_name=$1
   size=$2
 
@@ -96,12 +85,10 @@ drbd_add_res () {
   else
     $(drbdmanage add-volume $res_name $size)
   fi
-
 }
 
 # Deploy resource on a list of nodes, wait for res to be deployed on each node.
 drbd_deploy_res_on_nodes () {
-
   res_name=$1
 
   for node in "${@:2}"
@@ -109,12 +96,10 @@ drbd_deploy_res_on_nodes () {
     drbdmanage assign-resource $res_name $node
     drbd_wait_res_deployed $res_name $node
   done
-
 }
 
 # Deploy resource on virtualization host in diskless mode.
 drbd_deploy_res_on_host () {
-
     res_name=$1
     node_name=$2
 
@@ -124,7 +109,6 @@ drbd_deploy_res_on_host () {
 
 # Determine the size of a resource in bytes.
 drbd_get_res_size () {
-
   res_name=$1
 
   size_in_bytes=$(drbdmanage volumes -m --resources $res_name | awk -F',' '{ print $4 * 1024 }')
@@ -134,5 +118,4 @@ drbd_get_res_size () {
   else
     exit -1
   fi
-
 }
