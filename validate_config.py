88a2085107c5760019cb7381cf627388ad44de29
Hayley Swimelar <hayley@linbit.com>
2016-03-09 13:57:24 -0800

    update names of policies in validate_config.py

--- validate_config.py
+++ validate_config.py
@@ -83,32 +83,31 @@ if redundancy_level:
               "less than or equal to the number of nodes in BRIDGE_LIST")
 
 # Checks for optional attributes.
-
-if "DEPLOY_MIN_RATIO" in config:
+if "DRBD_MIN_RATIO" in config:
     try:
-        ratio = float(config["DEPLOY_MIN_RATIO"])
+        ratio = float(config["DRBD_MIN_RATIO"])
     except ValueError as e:
         valid_config = False
-        print("DEPLOY_MIN_RATIO must be a decimal number.")
+        print("DRBD_MIN_RATIO must be a decimal number.")
         report_validity()
         sys.exit(e)
 
     if not 0.0 <= ratio <= 1.0:
         valid_config = False
-        print("DEPLOY_MIN_RATIO must be between 0.0 and 1.0.")
+        print("DRBD_MIN_RATIO must be between 0.0 and 1.0.")
 
-if "DEPLOY_MIN_COUNT" in config:
+if "DRBD_MIN_COUNT" in config:
     try:
-        count = int(config["DEPLOY_MIN_COUNT"])
+        count = int(config["DRBD_MIN_COUNT"])
     except ValueError as e:
         valid_config = False
-        print("DEPLOY_MIN_COUNT must be an integer.")
+        print("DRBD_MIN_COUNT must be an integer.")
         report_validity()
         sys.exit(e)
 
     if not 0 <= count <= len(storage_nodes):
         valid_config = False
-        print("DEPLOY_MIN_COUNT must be between 0 and "
+        print("DRBD_MIN_COUNT must be between 0 and "
               "the number of storage nodes.")
 
 report_validity()
